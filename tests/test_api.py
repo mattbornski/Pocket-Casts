f9f14a5cd4690fe200d1780e856baf6d39532c4b
Matt Bornski <matt@bornski.com>
2018-10-11 09:46:16 -0700

    update tests and fixtures to use known podcast and episode info

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,9 @@ import pocketcasts
 import pytest
 import vcr
 
+PODCAST_UUID = '8b503540-25d2-0134-2f17-737688e4d168'
+EPISODE_UUID = 'a9037e26-c0cc-411c-8bb9-ae2fb2cd38fa'
+
 def test_invalid_method(pocket_instance):
     with pytest.raises(Exception):
         pocket_instance._make_req('test', method='INVALID')
@@ -20,14 +23,14 @@ def test_get_trending(pocket_instance):
     response = pocket_instance.get_trending()
 
 def test_get_podcast(pocket_instance):
-    response = pocket_instance.get_podcast('12012c20-0423-012e-f9a0-00163e1b201c')
+    response = pocket_instance.get_podcast(PODCAST_UUID)
 
 def test_get_podcast_episodes(pocket_instance):
     response = pocket_instance.get_podcast_episodes(pocket_instance.get_trending()[0])
 
 def test_get_episode(pocket_instance):
-    pod = pocket_instance.get_podcast("12012c20-0423-012e-f9a0-00163e1b201c")
-    pocket_instance.get_episode(pod, "7b28c700-d4f1-0134-ebdd-4114446340cb")
+    pod = pocket_instance.get_podcast(PODCAST_UUID)
+    pocket_instance.get_episode(pod, EPISODE_UUID)
 
 def test_get_starred(pocket_instance):
     pocket_instance.get_starred()
@@ -36,12 +39,12 @@ def test_search_podcasts(pocket_instance):
     pocket_instance.search_podcasts('test')
 
 def test_subscribe_functions(pocket_instance):
-    pod = pocket_instance.get_podcast("da9bb800-e230-0132-0bd1-059c869cc4eb")
+    pod = pocket_instance.get_podcast(PODCAST_UUID)
     pod.subscribed = True
     pod.subscribed = False
 
 def test_get_episode_notes(pocket_instance):
-    response = pocket_instance.get_episode_notes('a35748e0-bb4d-0134-10a8-25324e2a541d')
+    response = pocket_instance.get_episode_notes(EPISODE_UUID)
 
 def test_get_subscribed_podcasts(pocket_instance):
     response = pocket_instance.get_subscribed_podcasts()
@@ -53,31 +56,31 @@ def test_get_in_progress(pocket_instance):
     response = pocket_instance.get_in_progress()
 
 def test_update_playing_status(pocket_instance):
-    pod = pocket_instance.get_podcast("12012c20-0423-012e-f9a0-00163e1b201c")
+    pod = pocket_instance.get_podcast(PODCAST_UUID)
     epi = pocket_instance.get_podcast_episodes(pod)[-1]
     epi.playing_status = 3
 
 def test_invalid_update_playing_status(pocket_instance):
-    pod = pocket_instance.get_podcast("12012c20-0423-012e-f9a0-00163e1b201c")
+    pod = pocket_instance.get_podcast(PODCAST_UUID)
     epi = pocket_instance.get_podcast_episodes(pod)[-1]
     with pytest.raises(Exception) as context:
         epi.playing_status = 'invalid'
     assert 'Invalid status.' in str(context.value)
 
 def test_update_played_position(pocket_instance):
-    pod = pocket_instance.get_podcast("12012c20-0423-012e-f9a0-00163e1b201c")
+    pod = pocket_instance.get_podcast(PODCAST_UUID)
     epi = pocket_instance.get_podcast_episodes(pod)[-1]
     epi.played_up_to = 2
 
 def test_invalid_played_position(pocket_instance):
-    pod = pocket_instance.get_podcast("12012c20-0423-012e-f9a0-00163e1b201c")
+    pod = pocket_instance.get_podcast(PODCAST_UUID)
     epi = pocket_instance.get_podcast_episodes(pod)[-1]
     with pytest.raises(Exception) as context:
         epi.played_up_to = 'invalid'
     assert 'Sorry your update failed.' in str(context.value)
 
 def test_update_starred(pocket_instance):
-    pod = pocket_instance.get_podcast("12012c20-0423-012e-f9a0-00163e1b201c")
+    pod = pocket_instance.get_podcast(PODCAST_UUID)
     epi = pocket_instance.get_podcast_episodes(pod)[-1]
     epi.starred = True
     epi.starred = False
